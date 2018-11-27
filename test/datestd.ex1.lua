60337f09bb3a67b589db30424748fc9bb751f03a
user <no@mail.com>
2018-11-27 08:59:10 +0100

    FIX datestd: number of digit for the encoded seconds

--- test/datestd.ex1.lua
+++ test/datestd.ex1.lua
@@ -3,16 +3,19 @@ local datestd = require 'datestd'
 local t = require 'testhelper'
 
 t( datestd{}, '' )
-t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=10.1, zone=0}, '2018-01-21 14:32:10.100Z' )
+t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=10.1, zone=0}, '2018-01-21 14:32:10.1Z' )
 
-t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=10.1, zone=1}, '2018-01-21 14:32:10.100+01:00' )
-t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=10.1, zone=-1}, '2018-01-21 14:32:10.100-01:00' )
+t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=10.1, zone=1}, '2018-01-21 14:32:10.1+01:00' )
+t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=10.1, zone=-1}, '2018-01-21 14:32:10.1-01:00' )
 
-t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=10.1}, '2018-01-21 14:32:10.100' )
+t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=10.1}, '2018-01-21 14:32:10.1' )
 t( datestd{ year=2018, month=1, day=21}, '2018-01-21' )
-t( datestd{ hour=14, min=32, sec=10.1}, '14:32:10.100' )
+t( datestd{ hour=14, min=32, sec=10.1}, '14:32:10.1' )
 
--- t( datestd'', {}, t.deepsame )
+t( datestd{ year=2018, month=1, day=21, hour=14, min=32, sec=0}, '2018-01-21 14:32:00' )
+
+--
+
 t( datestd'2018-01-21 14:32:10.100Z', { year=2018, month=1, day=21, hour=14, min=32, sec=10.1, zone=0}, t.deepsame )
 
 t( datestd'2018-01-21 14:32:10.100+01:00', { year=2018, month=1, day=21, hour=14, min=32, sec=10.1, zone=1}, t.deepsame )
