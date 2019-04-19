ab9f48ac15e728ee54b655c0d73f79a71d646f4a
pocomane <none@mail.com>
2019-04-19 12:27:38 +0200

    FIX templua: test changes forgotten in previous commits

--- test/templua.ex1.lua
+++ test/templua.ex1.lua
@@ -15,7 +15,7 @@ m = templua( "@{{for i=1,3 do}} hello @{item}!@{{end}}" )
 t( ' hello world! hello world! hello world!', m{ item = 'world' } )
 
 -- Use the output function to expand text from complex lua code
-m = templua( "@{{for i=1,3 do o(' hello '..item..'!') end}}", 'o' )
+m = templua( "@{{for i=1,3 do _ENV[_ENV](' hello '..item..'!') end}}" )
 t( ' hello dude! hello dude! hello dude!', m{ item = 'dude' } )
 
 -- Last text appending
@@ -31,14 +31,14 @@ m = templua( "@{{][}}" )
 local s, e = m{}
 t( s, nil )
 t( e, 'Template script: ', t.patsame )
-t( e, '_o%(""%)%]%[ _o%(""%)', t.patsame )
+t( e, '_ENV%[_ENV%]%(""%)%]%[ _ENV%[_ENV%]%(""%)', t.patsame )
 
 -- The script is reported when a running error is found
 m = templua( "@{{undefined_function()}}" )
 local s, e = m{}
 t( s, nil )
 t( e, 'Template script: ', t.patsame )
-t( e, '_o%(""%)undefined_function%(%) _o%(""%)', t.patsame )
+t( e, '_ENV%[_ENV%]%(""%)undefined_function%(%) _ENV%[_ENV%]%(""%)', t.patsame )
 
 -- Nested template
 local s = {}
