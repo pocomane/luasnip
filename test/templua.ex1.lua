704cabdbc1dd272aad46fb2fef3aea6d279b26e8
user <no@mail.com>
2019-08-21 12:20:20 +0200

    IMPROVE templua: cleaner way to access the output function from lua statements in the template.

--- test/templua.ex1.lua
+++ test/templua.ex1.lua
@@ -8,14 +8,20 @@ t( 'ok', m{} )
 -- Lua expression expansion with @{}
 m = templua( "@{item.a} @{item.b:upper()}" )
 t( 'a B', m{ item = { a = 'a', b = 'b' } } )
+m = templua( "@{item.a} @{item.b:upper()}" )
 t( 'B A', m{ item = { a = 'B', b = 'a' } } )
+
+-- Call same template multiple times
+m = templua( "@{x}" )
+t( '1', m{ x=1 } )
+t( '2', m{ x=2 } )
 
 -- Mix lua statements and text with @{{}}
 m = templua( "@{{for i=1,3 do}} hello @{item}!@{{end}}" )
 t( ' hello world! hello world! hello world!', m{ item = 'world' } )
 
 -- Use the output function to expand text from complex lua code
-m = templua( "@{{for i=1,3 do _ENV[_ENV](' hello '..item..'!') end}}" )
+m = templua( "@{{for i=1,3 do out(' hello '..item..'!') end}}" )
 t( ' hello dude! hello dude! hello dude!', m{ item = 'dude' } )
 
 -- Last text appending
@@ -31,7 +37,7 @@ local m, e = templua( "@{{][}}" )
 t( m, nil )
 t( e, 'string "templua_script"%]:1: unexpected symbol', t.patsame )
 t( e, 'Template script: ', t.patsame )
-t( e, '_ENV%[_ENV%]%(""%)', t.patsame )
+t( e, 'out%(""%)', t.patsame )
 
 -- Error while expanding
 m = templua( "@{{undefined_function()}}" )
@@ -39,7 +45,7 @@ local s, e = m{}
 t( s, nil )
 t( e, 'string "templua_script"%]:1: attempt to call a nil value', t.patsame )
 t( e, 'Template script: ', t.patsame )
-t( e, '_ENV%[_ENV%]%(""%)', t.patsame )
+t( e, 'out%(""%)', t.patsame )
 
 -- Nested template
 local s = {}
